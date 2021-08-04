a8a75d82837cd1d91bd5d038c527470c30110b85
ericcapiz <ericcapiz@gmail.com>
2021-08-04 18:45:59 -0500

    small change

--- src/components/Header.js
+++ src/components/Header.js
@@ -30,20 +30,18 @@ const Header = () => {
                 <CloseWrapper>
                     <CustomClose onClick={()=>setMenuStatus(false)} />
                 </CloseWrapper>
+                {cars && cars.map((car, index)=> (
+                <li><a href="/">{car}</a></li>
+                ))}
                 <li><a href="/">Existing Inventory</a></li>
                 <li><a href="/">Used Inventory</a></li>
                 <li><a href="/">Trade-In</a></li>
-                <li><a href="/">Test Drive</a></li>
                 <li><a href="/">Cybertruck</a></li>
                 <li><a href="/">Roadster</a></li>
                 <li><a href="/">Semi</a></li>
                 <li><a href="/">Charging</a></li>
                 <li><a href="/">Powerwall</a></li>
-                <li><a href="/">Commercial Energy</a></li>
-                <li><a href="/">Utilities</a></li>
-                <li><a href="/">Find Us</a></li>
-                <li><a href="/">Support</a></li>
-                <li><a href="/">Investor Relations</a></li>
+                
             </BurgerNav>
         </Container>
     )
